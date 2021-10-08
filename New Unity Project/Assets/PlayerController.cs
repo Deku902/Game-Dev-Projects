11bd39da5edd11329536bb07147edd55b1c7b3cc
Alex <alexander.j757@stu.nebo.edu>
2021-10-08 15:11:24 -0600

    Fixed playerController Script

--- New Unity Project/Assets/PlayerController.cs
+++ New Unity Project/Assets/PlayerController.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 25.0f;
-    public float turnSpeed = 500.0f;
+    public float turnSpeed = 50.0f;
     public float hInput = 9.17f;
     public float vInput = 4.45f;
+
     public float xRange = 6.16f;
     public float yRange = 4.5f;
+
+    public GameObject projectile;
+    public Transform launcher;
+    public Vector3 offset = new Vector3(0,1,0);
+
+    // public float health;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,11 +32,32 @@ public class PlayerController : MonoBehaviour
         
         transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime * hInput);
         transform.Translate(Vector3.up * speed * Time.deltaTime * vInput);
-        //create right wall
-              if(transform.position.x > xRange)
+
+        //create a wall on the -x side
+              if(transform.position.x < -xRange)
               {
                   transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
               }
+        // Create a wall on x side
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3( xRange, transform.position.y, transform.position.z);
+        }
+
+        if(transform.position.y > yRange)
+        {
+            transform.position = new Vector3(transform.position.x, yRange, transform.position.z);
+        }
+
+        if(transform.position.y < -yRange)
+        {
+            transform.position = new Vector3(transform.position.x, -yRange, transform.position.z);
+        }
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(projectile, launcher.transform.position, launcher.transform.rotation);
+        }
     }
 
 }
