b80b1a42f3ab345a1f5ecd0554535bd575736947
Alex <alexander.j757@stu.nebo.edu>
2021-12-08 14:19:47 -0700

    UI Script

---
+++ Prototype 3 First Person Game/Assets/Scripts/GameUI.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagment;
+
+public class GameUI : MonoBehaviour
+{
+    [Header("hud")]
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI ammoText;
+    public Image healthBarFill
+
+   [Header("PauseMenu")]
+    public GameObject pauseMenu;
+
+    [Header("End Game Screen")]
+    public GameObject endGameScreen;
+    public TextMeshProUGUI endGameHeaderText;
+    public TextMeshProUGUI endGameScoreText;
+    
+    //Instance
+    public static GameUI instance;
+    void Awake()
+    {
+       //set the instance to this script
+       instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateHealthBar(int curHP, int maxHP)
+    {
+        healthBarFill.fillAmount = (float)curHP / (float)maxHP;
+    }
+
+    public void UpdateScoreText(int score)
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void UpdateAmmotext(int curAmmo, int maxAmmo)
+    {
+        ammoText.text = "Ammo: " + curAmmo + " / " + maxAmmo;
+    }
+    public void TogglePauseMenu(bool paused)
+    {
+        pauseMenu.SetActive(paused);
+    }
+
+    public void SetEngineGameScreen(bool won, int score)
+    {
+        endGameScreen.SetActive(true);
+        endGameHeaderText.text = won == true ? "You win, Eat taco bell like a king " : "You lose, No taco bell for you filty pesent";
+        endGameHeaderText.Color = won == true ? Color.brown : Color.red;
+        endGameScoreText.text = "<b>Score</b>\n" + score;
+    }
+
+    public void OnResumeButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void 
+
+
+}
